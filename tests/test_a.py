93b137583f25928232a65cf471a58a0a75de0863
Hans Donner <hans@hansdonner.nl>
2023-03-15 21:46:39 +0100

    PY-59546 - example

---
+++ tests/test_a.py
@@ -0,0 +1,8 @@
+import time
+
+
+def test_example_1():
+    time.sleep(2)
+
+def test_example_2():
+    time.sleep(10)
---
+++ tests/test_b.py
@@ -0,0 +1,8 @@
+import time
+
+
+def test_example_3():
+    time.sleep(2)
+
+def test_example_4():
+    time.sleep(3)
